22bd53fc93a048e722c2230f7abb2434271a5476
amirmahmoud <amir.alishavandi@live.com>
2017-02-23 08:49:51 -0800

    Initial Commit

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname+'/views/partials')
+app.set('view engine','hbs');
+
+app.use((req,res,next) => {
+  var now = new Date().toString();
+  var log = (`${now}: ${req.method} ${req.url}`);
+  fs.appendFile('server.log',log + '\n',(error) => {
+      if(error){console.log('Unable to ');}
+  })
+  console.log(`${now}: ${req.method} ${req.url}`);
+  next();
+});
+
+///maintainance mode
+// app.use((req,res) => {
+//   return res.render('maintainance.hbs')
+// });
+
+app.use(express.static(__dirname+'/public'));
+
+hbs.registerHelper('getCurrentYear',() => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt',(text) => {
+  return text.toUpperCase();
+});
+
+app.get('/',(req,res) => {
+  res.render('home.hbs',{
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Hey My friends, welcome to my new web page',
+})
+  // res.send('Root Page');
+});
+
+app.get('/about',(req,res) => {
+  res.render('about.hbs',{
+    pageTitle: 'About Page',
+})
+  // res.send('about page');
+});
+
+app.get('/bad',(req,res) => {
+  res.send({
+    errorMessage: 'Unable to get the Page'
+  });
+});
+
+app.listen(3000,() => {
+  console.log('starting web server on port 3000');
+});
